9f5da04b70bf7cc4a2b498b72923037cd64b6437
José Fernandes <95943327+jlfernandes22@users.noreply.github.com>
2026-03-20 14:34:37 +0000

    Correção Syntax

--- FrontEnd/app/components/CustomSnackBar.tsx
+++ FrontEnd/app/components/CustomSnackBar.tsx
@@ -39,27 +39,4 @@ const CustomSnackBar = ({
   );
 };
 
-        <Snackbar
-            visible={visible}
-            onDismiss={onDismiss} // Chama a função que passarmos do ecrã principal
-            duration={3000}
-            style={{
-              backgroundColor: isError ? '#DC2626' : '#16A34A', 
-              borderRadius: 16, 
-              marginHorizontal: 16 // Margem de segurança
-            }}
-            action={{
-              label: 'OK',
-              textColor: 'white', 
-              onPress: onDismiss, // Usa a mesma função para fechar
-            }}
-          >
-            <Text style={{ color: 'white', fontSize: 15, fontWeight: '500' }}>
-              {message}
-            </Text>
-          </Snackbar>
-
-  )
-}
-
 export default CustomSnackBar;
